8ddd43ad9d4f50d961fa2e16e16cfd86a4e74174
Ralf Hortt <me@horttcore.de>
2020-01-15 08:44:08 +0100

    Fixes and cleanup
    - Changed: Namespaced hooks
    - Changed: Removed redundant hooks
    - Fixed: Broke URL field
    
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- README.md
+++ README.md
@@ -34,22 +34,34 @@ use RalfHortt\MetaBoxContact\MetaBoxContact;
 (new MetaBoxContact(['page'], 'advanced', 'default' ))->register();
 ```
 
+## Fields
+
+- Phone
+- Fax
+- Mobile
+- E-Mail
+- URL
+
+```php
+<?php
+$phone = get_post_meta( $post->ID, 'contact-phone', TRUE );
+$fax = get_post_meta( $post->ID, 'contact-fax', TRUE );
+$mobile = get_post_meta( $post->ID, 'contact-mobile', TRUE );
+$email = get_post_meta( $post->ID, 'contact-email', TRUE );
+$url = get_post_meta( $post->ID, 'contact-url', TRUE );
+```
+
 ## Hooks
 
 ### Filter
 
-- `wp-meta-box-contact-meta-box-identifier` - Change meta box id
-- `wp-meta-box-contact-meta-box-label` - Change meta box label
-- `wp-meta-box-contact-phone` - Hide all phone fields
-- `wp-meta-box-contact-phone-{$postType}` - Hide phone field for \$postType
-- `wp-meta-box-contact-fax` - Hide all fax fields
-- `wp-meta-box-contact-fax-{$postType}` - Hide fax field for \$postType
-- `wp-meta-box-contact-mobile` - Hide all mobile fields
-- `wp-meta-box-contact-mobile-{$postType}` - Hide mobile field for \$postType
-- `wp-meta-box-contact-email` - Hide all email fields
-- `wp-meta-box-contact-email-{$postType}` - Hide email field for \$postType
-- `wp-meta-box-contact-url` - Hide all url fields
-- `wp-meta-box-contact-url-{$postType}` - Hide url field for \$postType
+- `wp-meta-box-contact/identifier` - Change meta box id
+- `wp-meta-box-contact/label` - Change meta box label
+- `wp-meta-box-contact/phone-{$postType}` - Hide phone field for \$postType
+- `wp-meta-box-contact/fax-{$postType}` - Hide fax field for \$postType
+- `wp-meta-box-contact/mobile-{$postType}` - Hide mobile field for \$postType
+- `wp-meta-box-contact/email-{$postType}` - Hide email field for \$postType
+- `wp-meta-box-contact/url-{$postType}` - Hide url field for \$postType
 
 #### Example
 
@@ -60,11 +72,22 @@ add_filter('wp-meta-box-contact-phone', '__return_false');
 
 ## Action
 
-- `wp-meta-contact-data-before` - Add fields before the phone field
-- `wp-meta-contact-data-after` - Add fields after the url field
-- `wp-meta-contact-save` - Save custom fields
+- `wp-meta-contact/before` - Add fields before the phone field
+- `wp-meta-contact/after` - Add fields after the url field
+- `wp-meta-contact/save` - Save custom fields
+
+## ToDo
+
+- Template tags
+- REST API Support
 
 ## Changelog
+
+### v.2.0.0 - 2020-01-15
+
+- Changed: Namespaced hooks
+- Changed: Removed redundant hooks
+- Fixed: Broke URL field
 
 ### v1.0.0 - 2020-01-14
 
--- src/MetaBoxContact.php
+++ src/MetaBoxContact.php
@@ -9,8 +9,8 @@ class MetaBoxContact extends MetaBox
     
     function __construct(array $screen = [], string $context = 'advanced', string $priority = 'default')
     {
-        $this->identifier = apply_filters( 'wp-meta-box-contact-meta-box-identifier', 'contact-data');
-        $this->name = apply_filters( 'wp-meta-box-contact-meta-box-label', __('Contact', 'wp-meta-box-contact'));
+        $this->identifier = apply_filters( 'wp-meta-box-contact/identifier', 'contact-data');
+        $this->name = apply_filters( 'wp-meta-box-contact/label', __('Contact', 'wp-meta-box-contact'));
         $this->screen = $screen;
         $this->context = $context;
         $this->priority = $priority;
@@ -33,44 +33,44 @@ class MetaBoxContact extends MetaBox
 		?>
         <table class="form-table">
 
-            <?php do_action( 'wp-meta-contact-data-before' ) ?>
+            <?php do_action( 'wp-meta-contact/before' ) ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact-phone',  true) || apply_filters('wp-meta-box-contact-phone-' . $post->post_type, true ) ) { ?>
+            <?php if ( apply_filters('wp-meta-box-contact/phone-' . $post->post_type, true ) ) { ?>
                 <tr>
                     <th><label for="wp-meta-box-contact-phone"><?php _e( 'Phone', 'wp-meta-box-contact' ) ?></label></th>
-                    <td><input type="text" name="wp-meta-box-contact-phone" id="wp-meta-box-contact-phone" value="<?= esc_attr($phone) ?>"></td>
+                    <td><input class="regular-text" type="text" name="wp-meta-box-contact-phone" id="wp-meta-box-contact-phone" value="<?= esc_attr($phone) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact-fax',  true) || apply_filters('wp-meta-box-contact-fax-' . $post->post_type, true ) ) { ?>
+            <?php if ( apply_filters('wp-meta-box-contact/fax-' . $post->post_type, true ) ) { ?>
                 <tr>
                     <th><label for="wp-meta-box-contact-fax"><?php _e( 'Fax', 'wp-meta-box-contact' ) ?></label></th>
-                    <td><input type="text" name="wp-meta-box-contact-fax" id="wp-meta-box-contact-fax" value="<?= esc_attr($fax) ?>"></td>
+                    <td><input class="regular-text" type="text" name="wp-meta-box-contact-fax" id="wp-meta-box-contact-fax" value="<?= esc_attr($fax) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact-mobile',  true) || apply_filters('wp-meta-box-contact-mobile-' . $post->post_type, true ) ) { ?>
+            <?php if ( apply_filters('wp-meta-box-contact/mobile-' . $post->post_type, true ) ) { ?>
                 <tr>
                     <th><label for="wp-meta-box-contact-mobile"><?php _e( 'Mobile', 'wp-meta-box-contact' ) ?></label></th>
-                    <td><input type="text" name="wp-meta-box-contact-mobile" id="wp-meta-box-contact-mobile" value="<?= esc_attr($mobile) ?>"></td>
+                    <td><input class="regular-text" type="text" name="wp-meta-box-contact-mobile" id="wp-meta-box-contact-mobile" value="<?= esc_attr($mobile) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact-email',  true) || apply_filters('wp-meta-box-contact-email-' . $post->post_type, true ) ) { ?>
+            <?php if ( apply_filters('wp-meta-box-contact/email-' . $post->post_type, true ) ) { ?>
                 <tr>
                     <th><label for="wp-meta-box-contact-email"><?php _e( 'E-Mail', 'wp-meta-box-contact' ) ?></label></th>
-                    <td><input type="email" name="wp-meta-box-contact-email" id="wp-meta-box-contact-email" value="<?= esc_attr($email) ?>"></td>
+                    <td><input class="regular-text" type="email" name="wp-meta-box-contact-email" id="wp-meta-box-contact-email" value="<?= esc_attr($email) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact-email',  true) || apply_filters('wp-meta-box-contact-email-' . $post->post_type, true ) ) { ?>
+            <?php if ( apply_filters('wp-meta-box-contact/url-' . $post->post_type, true ) ) { ?>
                 <tr>
                     <th><label for="wp-meta-box-contact-url"><?php _e( 'URL', 'wp-meta-box-contact' ) ?></label></th>
-                    <td><input type="url" name="wp-meta-box-contact-url" id="wp-meta-box-contact-url" value="<?= esc_attr($url) ?>"></td>
+                    <td><input class="regular-text" type="url" name="wp-meta-box-contact-url" id="wp-meta-box-contact-url" value="<?= esc_attr($url) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php do_action( 'wp-meta-contact-data-after' ) ?>
+            <?php do_action( 'wp-meta-box-contact/after' ) ?>
 
         </table>
 		<?php
@@ -78,23 +78,23 @@ class MetaBoxContact extends MetaBox
 
 	function save(int $postId): void
 	{
-        if ( apply_filters('wp-meta-box-contact-phone',  true) || apply_filters('wp-meta-box-contact-phone-' . $post->post_type, true) ) {
+        if ( apply_filters('wp-meta-box-contact-phone-' . $post->post_type, true) ) {
             update_post_meta( $postId, '_contact-phone', sanitize_text_field($_POST['wp-meta-box-contact-phone']) );
         }
 
-        if ( apply_filters('wp-meta-box-contact-fax',  true) || apply_filters('wp-meta-box-contact-fax-' . $post->post_type, true) ) {
+        if ( apply_filters('wp-meta-box-contact-fax-' . $post->post_type, true) ) {
             update_post_meta( $postId, '_contact-fax', sanitize_text_field($_POST['wp-meta-box-contact-fax']) );
         }
 
-        if ( apply_filters('wp-meta-box-contact-mobile',  true) || apply_filters('wp-meta-box-contact-mobile-' . $post->post_type, true) ) {
+        if ( apply_filters('wp-meta-box-contact-mobile-' . $post->post_type, true) ) {
             update_post_meta( $postId, '_contact-mobile', sanitize_text_field($_POST['wp-meta-box-contact-mobile']) );
         }
 
-        if ( apply_filters('wp-meta-box-contact-email',  true) || apply_filters('wp-meta-box-contact-email-' . $post->post_type, true) ) {
+        if ( apply_filters('wp-meta-box-contact-email-' . $post->post_type, true) ) {
             update_post_meta( $postId, '_contact-email', sanitize_email($_POST['wp-meta-box-contact-email']) );
         }
 
-        if ( apply_filters('wp-meta-box-contact-url',  true) || apply_filters('wp-meta-box-contact-email-' . $post->post_type, true) ) {
+        if ( apply_filters('wp-meta-box-contact-url-' . $post->post_type, true) ) {
             update_post_meta( $postId, '_contact-url', esc_url_raw($_POST['wp-meta-box-contact-url']) );
         }
 
