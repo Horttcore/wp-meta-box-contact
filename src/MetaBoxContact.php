f5433b256e5e290558ad155e24253c217b54767e
Ralf Hortt <me@horttcore.de>
2020-05-25 09:20:19 +0000

    Apply fixes from StyleCI

--- src/MetaBoxContact.php
+++ src/MetaBoxContact.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace RalfHortt\MetaBoxContact;
 
 use Horttcore\MetaBoxes\MetaBox;
@@ -6,11 +7,10 @@ use RalfHortt\TranslatorService\Translator;
 
 class MetaBoxContact extends MetaBox
 {
-    
-    function __construct(array $screen = [], string $context = 'advanced', string $priority = 'default')
+    public function __construct(array $screen = [], string $context = 'advanced', string $priority = 'default')
     {
-        $this->identifier = apply_filters( 'wp-meta-box-contact/identifier', 'contact-data');
-        $this->name = apply_filters( 'wp-meta-box-contact/label', __('Contact', 'wp-meta-box-contact'));
+        $this->identifier = apply_filters('wp-meta-box-contact/identifier', 'contact-data');
+        $this->name = apply_filters('wp-meta-box-contact/label', __('Contact', 'wp-meta-box-contact'));
         $this->screen = $screen;
         $this->context = $context;
         $this->priority = $priority;
@@ -23,81 +23,80 @@ class MetaBoxContact extends MetaBox
         (new Translator('wp-meta-box-contact', dirname(plugin_basename(__FILE__)).'/languages/'))->register();
     }
 
-	function render(\WP_Post $post): void 
-	{
-		$phone = get_post_meta( $post->ID, '_contact-phone', TRUE );
-		$fax = get_post_meta( $post->ID, '_contact-fax', TRUE );
-		$mobile = get_post_meta( $post->ID, '_contact-mobile', TRUE );
-		$email = get_post_meta( $post->ID, '_contact-email', TRUE );
-		$url = get_post_meta( $post->ID, '_contact-url', TRUE );
-		?>
+    public function render(\WP_Post $post): void
+    {
+        $phone = get_post_meta($post->ID, '_contact-phone', true);
+        $fax = get_post_meta($post->ID, '_contact-fax', true);
+        $mobile = get_post_meta($post->ID, '_contact-mobile', true);
+        $email = get_post_meta($post->ID, '_contact-email', true);
+        $url = get_post_meta($post->ID, '_contact-url', true); ?>
         <table class="form-table">
 
-            <?php do_action( 'wp-meta-contact/before', $post ) ?>
+            <?php do_action('wp-meta-contact/before', $post) ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact/phone-' . $post->post_type, true ) ) { ?>
+            <?php if (apply_filters('wp-meta-box-contact/phone-'.$post->post_type, true)) { ?>
                 <tr>
-                    <th><label for="wp-meta-box-contact-phone"><?php _e( 'Phone', 'wp-meta-box-contact' ) ?></label></th>
+                    <th><label for="wp-meta-box-contact-phone"><?php _e('Phone', 'wp-meta-box-contact') ?></label></th>
                     <td><input class="regular-text" type="text" name="wp-meta-box-contact-phone" id="wp-meta-box-contact-phone" value="<?= esc_attr($phone) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact/fax-' . $post->post_type, true ) ) { ?>
+            <?php if (apply_filters('wp-meta-box-contact/fax-'.$post->post_type, true)) { ?>
                 <tr>
-                    <th><label for="wp-meta-box-contact-fax"><?php _e( 'Fax', 'wp-meta-box-contact' ) ?></label></th>
+                    <th><label for="wp-meta-box-contact-fax"><?php _e('Fax', 'wp-meta-box-contact') ?></label></th>
                     <td><input class="regular-text" type="text" name="wp-meta-box-contact-fax" id="wp-meta-box-contact-fax" value="<?= esc_attr($fax) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact/mobile-' . $post->post_type, true ) ) { ?>
+            <?php if (apply_filters('wp-meta-box-contact/mobile-'.$post->post_type, true)) { ?>
                 <tr>
-                    <th><label for="wp-meta-box-contact-mobile"><?php _e( 'Mobile', 'wp-meta-box-contact' ) ?></label></th>
+                    <th><label for="wp-meta-box-contact-mobile"><?php _e('Mobile', 'wp-meta-box-contact') ?></label></th>
                     <td><input class="regular-text" type="text" name="wp-meta-box-contact-mobile" id="wp-meta-box-contact-mobile" value="<?= esc_attr($mobile) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact/email-' . $post->post_type, true ) ) { ?>
+            <?php if (apply_filters('wp-meta-box-contact/email-'.$post->post_type, true)) { ?>
                 <tr>
-                    <th><label for="wp-meta-box-contact-email"><?php _e( 'E-Mail', 'wp-meta-box-contact' ) ?></label></th>
+                    <th><label for="wp-meta-box-contact-email"><?php _e('E-Mail', 'wp-meta-box-contact') ?></label></th>
                     <td><input class="regular-text" type="email" name="wp-meta-box-contact-email" id="wp-meta-box-contact-email" value="<?= esc_attr($email) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php if ( apply_filters('wp-meta-box-contact/url-' . $post->post_type, true ) ) { ?>
+            <?php if (apply_filters('wp-meta-box-contact/url-'.$post->post_type, true)) { ?>
                 <tr>
-                    <th><label for="wp-meta-box-contact-url"><?php _e( 'URL', 'wp-meta-box-contact' ) ?></label></th>
+                    <th><label for="wp-meta-box-contact-url"><?php _e('URL', 'wp-meta-box-contact') ?></label></th>
                     <td><input class="regular-text" type="url" name="wp-meta-box-contact-url" id="wp-meta-box-contact-url" value="<?= esc_attr($url) ?>"></td>
                 </tr>
             <?php } ?>
 
-            <?php do_action( 'wp-meta-box-contact/after', $post ) ?>
+            <?php do_action('wp-meta-box-contact/after', $post) ?>
 
         </table>
 		<?php
-	}
+    }
 
-	function save(int $postId): void
-	{
-        if ( apply_filters('wp-meta-box-contact-phone-' . $post->post_type, true) ) {
-            update_post_meta( $postId, '_contact-phone', sanitize_text_field($_POST['wp-meta-box-contact-phone']) );
+    public function save(int $postId): void
+    {
+        if (apply_filters('wp-meta-box-contact-phone-'.$post->post_type, true)) {
+            update_post_meta($postId, '_contact-phone', sanitize_text_field($_POST['wp-meta-box-contact-phone']));
         }
 
-        if ( apply_filters('wp-meta-box-contact-fax-' . $post->post_type, true) ) {
-            update_post_meta( $postId, '_contact-fax', sanitize_text_field($_POST['wp-meta-box-contact-fax']) );
+        if (apply_filters('wp-meta-box-contact-fax-'.$post->post_type, true)) {
+            update_post_meta($postId, '_contact-fax', sanitize_text_field($_POST['wp-meta-box-contact-fax']));
         }
 
-        if ( apply_filters('wp-meta-box-contact-mobile-' . $post->post_type, true) ) {
-            update_post_meta( $postId, '_contact-mobile', sanitize_text_field($_POST['wp-meta-box-contact-mobile']) );
+        if (apply_filters('wp-meta-box-contact-mobile-'.$post->post_type, true)) {
+            update_post_meta($postId, '_contact-mobile', sanitize_text_field($_POST['wp-meta-box-contact-mobile']));
         }
 
-        if ( apply_filters('wp-meta-box-contact-email-' . $post->post_type, true) ) {
-            update_post_meta( $postId, '_contact-email', sanitize_email($_POST['wp-meta-box-contact-email']) );
+        if (apply_filters('wp-meta-box-contact-email-'.$post->post_type, true)) {
+            update_post_meta($postId, '_contact-email', sanitize_email($_POST['wp-meta-box-contact-email']));
         }
 
-        if ( apply_filters('wp-meta-box-contact-url-' . $post->post_type, true) ) {
-            update_post_meta( $postId, '_contact-url', esc_url_raw($_POST['wp-meta-box-contact-url']) );
+        if (apply_filters('wp-meta-box-contact-url-'.$post->post_type, true)) {
+            update_post_meta($postId, '_contact-url', esc_url_raw($_POST['wp-meta-box-contact-url']));
         }
 
-        do_action( 'wp-meta-contact-save', $postId, $post);
-	}
+        do_action('wp-meta-contact-save', $postId, $post);
+    }
 }
